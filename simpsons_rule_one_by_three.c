c01c4405dffcd01f726694b5ceb45f05d42d77fe
iamdhrohan <rohan@rohan.com>
2024-03-12 08:56:35 +0545

    3/8 1/3 simpson's rule added

---
+++ simpsons_rule_one_by_three.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<math.h>
+#define e 2.71828
+#define f(x) pow(e, x)
+
+int main(){
+    int  interval, i = 1, mode = 0;
+    float sum, l_lim, u_lim, h, result, x;
+
+    printf("Enter the value subinterval: ");
+    scanf("%d", &interval);
+
+    float y[interval+1];
+
+    printf("Enter the upper and lower limit (U-L): ");
+    scanf("%f%f", &u_lim, &l_lim);
+
+    h = (u_lim-l_lim)/interval;
+    x = l_lim;
+    y[0] = f(l_lim);
+
+    while (x<u_lim){
+        y[i] = f(x + h);
+        printf("%.2f\n", f(x+h));
+        x += h;
+        i++;
+    }
+    sum = y[0]+ y[interval];
+
+    for(i=1; i<interval; i++){
+        if(mode == 0){
+            sum += 4*y[i];
+            mode += 2;
+        }else{
+            sum += 2*y[i];
+            mode -= 2;
+        }
+    }
+
+    result = (h/3)*sum;
+    printf("The area under the given point of the curve is %.2f", result);
+}
